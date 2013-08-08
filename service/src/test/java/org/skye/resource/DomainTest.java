55c40d292ce1d86d3b30bce1a1922ecdc300b936
Philip Dodds <philip.dodds@me.com>
2013-08-08 00:11:07 -0400

    Adding in the basics for the testing framework

---
+++ service/src/test/java/org/skye/resource/DomainTest.java
@@ -0,0 +1,25 @@
+package org.skye.resource;
+
+import org.junit.Test;
+import org.skye.domain.Domain;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test the serialization and handling of the {@link Domain}
+ */
+public class DomainTest {
+
+    @Test
+    public void serializesToJSON() throws Exception {
+        final Domain domain = new Domain();
+        domain.setName("Example");
+        assertThat("a Domain can be serialized to JSON",
+                asJson(domain),
+                is(equalTo(jsonFixture("fixtures/domain.json"))));
+    }
+}
