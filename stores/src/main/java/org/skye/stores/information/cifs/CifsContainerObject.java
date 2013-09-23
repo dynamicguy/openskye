54ad909ebbf8733cfc171c6b2343448bf0079f44
Matt Taylor <nickylogic@gmail.com>
2013-09-23 17:31:09 -0500

    CIFS info store first cut

---
+++ stores/src/main/java/org/skye/stores/information/cifs/CifsContainerObject.java
@@ -0,0 +1,15 @@
+package org.skye.stores.information.cifs;
+
+import jcifs.smb.SmbFile;
+import org.skye.core.ContainerObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mftaylor
+ * Date: 9/23/13
+ * Time: 5:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CifsContainerObject extends ContainerObject {
+    public SmbFile smbFile;
+}
