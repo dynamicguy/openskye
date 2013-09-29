5f6e6b8b8233957de2f4210e97d98a238aa207a5
Philip Dodds <philip.dodds@me.com>
2013-09-29 12:42:15 -0400

    More little fixes in the handling of the ACB

--- core/src/main/java/org/skye/core/ArchiveContentBlock.java
+++ core/src/main/java/org/skye/core/ArchiveContentBlock.java
@@ -2,13 +2,15 @@ package org.skye.core;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 /**
  * A block of content that relates to a {@link SimpleObject}
  */
 @Data
 public class ArchiveContentBlock {
 
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String archiveStoreDefinitionId;
 
 }
