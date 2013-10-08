d7b3d702f09909c9cbabc4517ab57115a4e7a8a0
Philip Dodds <philip.dodds@me.com>
2013-10-08 09:56:29 -0700

    Slowly wiring in Shiro again

--- service/src/main/java/org/skye/SkyeApplication.java
+++ service/src/main/java/org/skye/SkyeApplication.java
@@ -1,20 +1,32 @@
 package org.skye;
 
+import com.google.common.base.Function;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.persist.PersistFilter;
 import com.google.inject.persist.jpa.JpaPersistModule;
+import com.google.inject.servlet.GuiceFilter;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import lombok.extern.slf4j.Slf4j;
 import org.skye.config.SkyeConfiguration;
-import org.skye.guice.GuiceBundle;
+import org.skye.guice.*;
 import org.skye.util.SwaggerBundle;
+
+import javax.annotation.Nullable;
+import java.util.Properties;
 
 /**
  * The Skye Service
  */
 @Slf4j
 public class SkyeApplication extends Application<SkyeConfiguration> {
+
+    private Bootstrap<SkyeConfiguration> bootstrap;
 
     public static void main(String[] args) throws Exception {
         new SkyeApplication().run(args);
@@ -25,28 +37,51 @@ public class SkyeApplication extends Application<SkyeConfiguration> {
         bootstrap.addBundle(new AssetsBundle("/apidocs", "/explore", "index.html"));
         bootstrap.addBundle(new SwaggerBundle());
 
-        // Set-up Guice
-        // Set-up the persistence
-        JpaPersistModule jpaPersistModule = new JpaPersistModule("Default");
-//        Properties props = new Properties();
-//        props.put("javax.persistence.jdbc.url", configuration.getDatabaseConfiguration().getUrl());
-//        props.put("javax.persistence.jdbc.user", configuration.getDatabaseConfiguration().getUser());
-//        props.put("javax.persistence.jdbc.password", configuration.getDatabaseConfiguration().getPassword());
-//        props.put("javax.persistence.jdbc.driver", configuration.getDatabaseConfiguration().getDriverClass());
-//        jpaPersistModule.properties(props);
-
-        bootstrap.addBundle(GuiceBundle.newBuilder()
-                .addModule(jpaPersistModule)
-                .enableAutoConfig(getClass().getPackage().getName())
-                .build()
-        );
-
+        this.bootstrap = bootstrap;
     }
 
     @Override
     public void run(SkyeConfiguration configuration,
                     Environment environment) throws Exception {
 
+        final GuiceContainer container = new GuiceContainer();
+        JerseyContainerModule jerseyContainerModule = new JerseyContainerModule(container);
+
+        // Set-up the persistence
+        JpaPersistModule jpaPersistModule = new JpaPersistModule("Default");
+        Properties props = new Properties();
+        props.put("javax.persistence.jdbc.url", configuration.getDatabaseConfiguration().getUrl());
+        props.put("javax.persistence.jdbc.user", configuration.getDatabaseConfiguration().getUser());
+        props.put("javax.persistence.jdbc.password", configuration.getDatabaseConfiguration().getPassword());
+        props.put("javax.persistence.jdbc.driver", configuration.getDatabaseConfiguration().getDriverClass());
+        jpaPersistModule.properties(props);
+
+        DropwizardEnvironmentModule<SkyeConfiguration> dropwizardEnvironmentModule = new DropwizardEnvironmentModule<>(SkyeConfiguration.class);
+
+        Injector injector = Guice.createInjector(jerseyContainerModule, dropwizardEnvironmentModule, jpaPersistModule);
+
+        AutoConfig autoConfig = new AutoConfig(this.getClass().getPackage().getName());
+        autoConfig.initialize(bootstrap, injector);
+
+        container.setResourceConfig(environment.jersey().getResourceConfig());
+        environment.jersey().replace(new Function<ResourceConfig, ServletContainer>() {
+            @Nullable
+            @Override
+            public ServletContainer apply(ResourceConfig resourceConfig) {
+                return container;
+            }
+        });
+        environment.servlets().addFilter("Guice Filter", GuiceFilter.class)
+                .addMappingForUrlPatterns(null, false, environment.getApplicationContext().getContextPath() + "*");
+
+        // Complete the autoconfig
+        autoConfig.run(environment, injector);
+
+        environment.servlets().addFilter("Guice Persist Filter", injector.getInstance(PersistFilter.class))
+                .addMappingForUrlPatterns(null, false, environment.getApplicationContext().getContextPath() + "*");
+
+        // Add a listener for us to be able to wire in Shiro
+        environment.servlets().addServletListeners(new SkyeGuiceServletContextListener(jpaPersistModule));
 
     }
 
