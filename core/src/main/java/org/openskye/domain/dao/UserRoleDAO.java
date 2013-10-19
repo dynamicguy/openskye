6d109c80a457bfe10d536058c256cb028a832d05
Matt Taylor <nickylogic@gmail.com>
2013-10-19 16:10:23 -0500

    Add userRoles API

---
+++ core/src/main/java/org/openskye/domain/dao/UserRoleDAO.java
@@ -0,0 +1,11 @@
+package org.openskye.domain.dao;
+
+import org.openskye.domain.UserRole;
+
+/**
+ * DAO for the {@link org.openskye.domain.UserRole}
+ */
+public class UserRoleDAO extends AbstractPaginatingDAO<UserRole> {
+
+
+}
---
+++ service/src/main/java/org/openskye/resource/UserRoleResource.java
@@ -0,0 +1,69 @@
+package org.openskye.resource;
+
+import com.codahale.metrics.annotation.Timed;
+import com.google.inject.persist.Transactional;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.openskye.domain.User;
+import org.openskye.domain.UserRole;
+import org.openskye.domain.dao.AbstractPaginatingDAO;
+import org.openskye.domain.dao.PaginatedResult;
+import org.openskye.domain.dao.UserRoleDAO;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+/**
+ * The REST endpoint for {@link org.openskye.domain.UserRole}
+ */
+@Api(value = "/api/1/userRoles", description = "Manage user roles")
+@Path("/api/1/userRoles")
+public class UserRoleResource extends AbstractUpdatableDomainResource<UserRole> {
+
+    private UserRoleDAO userRoleDAO;
+
+    @Inject
+    public UserRoleResource(UserRoleDAO dao) {
+        this.userRoleDAO = dao;
+    }
+
+    @ApiOperation(value = "List all user roles", notes = "Returns all user roles in a paginated structure", responseContainer = "List", response = UserRole.class)
+    @GET
+    @Transactional
+    @Timed
+    @Override
+    public PaginatedResult<UserRole> getAll() {
+        return super.getAll();
+    }
+
+    @ApiOperation(value = "Add new user role", response = UserRole.class)
+    @POST
+    @Transactional
+    @Timed
+    @Override
+    public UserRole create(UserRole newInstance) {
+        return super.create(newInstance);
+    }
+
+    @ApiOperation(value = "Delete user role", notes = "Delete the user role")
+    @Path("/{id}")
+    @DELETE
+    @Transactional
+    @Timed
+    @Override
+    public Response delete(@PathParam("id") String id) {
+        return super.delete(id);
+    }
+
+    @Override
+    protected AbstractPaginatingDAO<UserRole> getDAO() {
+        return userRoleDAO;
+    }
+
+    @Override
+    protected String getPermissionDomain() {
+        return "userRole";
+    }
+
+}
