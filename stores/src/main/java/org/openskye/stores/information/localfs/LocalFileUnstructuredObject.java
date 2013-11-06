cfc3497e8ab443585c7982fd0545f596e7e7ac0d
Philip Dodds <philip.dodds@me.com>
2013-11-06 15:03:24 -0800

    Fix up the local fs NPE problem and ensure we get file size and mime-type

--- stores/src/main/java/org/openskye/stores/information/localfs/LocalFileUnstructuredObject.java
+++ stores/src/main/java/org/openskye/stores/information/localfs/LocalFileUnstructuredObject.java
@@ -1,5 +1,6 @@
 package org.openskye.stores.information.localfs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openskye.core.MissingObjectException;
 import org.openskye.core.UnstructuredObject;
 
@@ -11,11 +12,13 @@ import java.io.InputStream;
  * An implementation of an {@link UnstructuredObject} that can be used
  * for local files
  */
+@Slf4j
 public class LocalFileUnstructuredObject extends UnstructuredObject {
 
     @Override
     public InputStream getContent() throws MissingObjectException {
         try {
+            log.info("Getting content on " + getObjectMetadata());
             InputStream is = new FileInputStream(getObjectMetadata().getPath());
             return is;
         } catch (FileNotFoundException e) {
--- stores/src/test/java/org/openskye/stores/information/localfs/LocalFSInformationStoreTest.java
+++ stores/src/test/java/org/openskye/stores/information/localfs/LocalFSInformationStoreTest.java
@@ -1,17 +1,21 @@
 package org.openskye.stores.information.localfs;
 
 import com.google.guiceberry.junit4.GuiceBerryRule;
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openskye.domain.*;
+import org.openskye.metadata.ObjectMetadataRepository;
 import org.openskye.stores.StoreRegistry;
 import org.openskye.stores.archive.localfs.LocalFSArchiveStore;
 import org.openskye.stores.information.InMemoryTestModule;
-import org.openskye.stores.inmemory.InMemoryArchiveStore;
 import org.openskye.task.TaskManager;
 
 import javax.inject.Inject;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -23,38 +27,38 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class LocalFSInformationStoreTest {
 
+    private static File archiveDirectory = new File("/tmp/" + UUID.randomUUID().toString());
+    private static File sourceDirectory = new File("/tmp/" + UUID.randomUUID().toString());
     @Rule
     public final GuiceBerryRule guiceBerry = new GuiceBerryRule(InMemoryTestModule.class);
     @Inject
     public TaskManager taskManager;
     @Inject
     public StoreRegistry registry;
+    @Inject
+    public ObjectMetadataRepository omr;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        FileUtils.forceMkdir(archiveDirectory);
+        FileUtils.forceMkdir(sourceDirectory);
+        FileUtils.forceDeleteOnExit(archiveDirectory);
+        FileUtils.forceDeleteOnExit(sourceDirectory);
+
+        InputStream bookStream = LocalFSInformationStoreTest.class.getClassLoader().getResourceAsStream("book.txt");
+        FileUtils.copyInputStreamToFile(bookStream, new File(sourceDirectory.getAbsolutePath() + "/alice.txt"));
+    }
 
     public InformationStoreDefinition getLocalFsDis() {
         InformationStoreDefinition dis = new InformationStoreDefinition();
         dis.setId(UUID.randomUUID().toString());
         dis.setImplementation(LocalFSInformationStore.IMPLEMENTATION);
-        dis.getProperties().put(LocalFSInformationStore.FILE_PATH, "/opt");
-        dis.getProperties().put(LocalFSInformationStore.NAME, "Linux /opt Directory");
+        dis.getProperties().put(LocalFSInformationStore.FILE_PATH, sourceDirectory.getAbsolutePath());
+        dis.getProperties().put(LocalFSInformationStore.NAME, "Example source");
         return dis;
     }
 
-
-    public ArchiveStoreDefinition getInMemoryArchive()
-    {
-        ArchiveStoreInstance asi = new ArchiveStoreInstance();
-        asi.setId(UUID.randomUUID().toString());
-        asi.setImplementation(InMemoryArchiveStore.IMPLEMENTATION);
-
-        ArchiveStoreDefinition asd = new ArchiveStoreDefinition();
-        asd.setId(UUID.randomUUID().toString());
-        asd.setArchiveStoreInstance(asi);
-
-        return asd;
-    }
-
-    public ArchiveStoreDefinition getLocalFsArchive(String path)
-    {
+    public ArchiveStoreDefinition getLocalFsArchive(String path) {
         ArchiveStoreInstance asi = new ArchiveStoreInstance();
         asi.setId(UUID.randomUUID().toString());
         asi.setImplementation(LocalFSArchiveStore.IMPLEMENTATION);
@@ -75,10 +79,8 @@ public class LocalFSInformationStoreTest {
     @Test
     public void ensureWeCanDiscoverObjects() {
         assertThat("Get metadata for the store", registry.build(getLocalFsDis()).get().getMetadata() != null);
-        LocalFSInformationStore is = (LocalFSInformationStore) registry.build(getLocalFsDis()).get();
-
         InformationStoreDefinition dis = getLocalFsDis();
-        ArchiveStoreDefinition das = getLocalFsArchive("/tmp/pj2");
+        ArchiveStoreDefinition das = getLocalFsArchive(archiveDirectory.getAbsolutePath());
         ChannelArchiveStore cas = new ChannelArchiveStore();
         cas.setArchiveStoreDefinition(das);
         Channel channel = new Channel();
@@ -102,36 +104,6 @@ public class LocalFSInformationStoreTest {
 
         long objectCount = newTask.getStatistics().getSimpleObjectsDiscovered();
         assertThat("We have discovered " + objectCount + " simple objects", objectCount > 0);
-    }
-
-    @Test
-    public void ensureWeCanArchiveToALocalFS() {
-
-        InformationStoreDefinition dis = getLocalFsDis();
-        ArchiveStoreDefinition das = getInMemoryArchive();
-        ChannelArchiveStore cas = new ChannelArchiveStore();
-        cas.setArchiveStoreDefinition(das);
-        Channel channel = new Channel();
-        channel.getChannelArchiveStores().add(cas);
-        channel.setInformationStoreDefinition(dis);
-        channel.setId(UUID.randomUUID().toString());
-        ArchiveStoreInstance asi = das.getArchiveStoreInstance();
-
-        // Test that the objects have valid ids.
-        assertThat("the information store definition has a valid id",
-                (dis.getId() != null && dis.getId().isEmpty() == false));
-
-        assertThat("the archive store definition has a valid id",
-                (das.getId() != null && das.getId().isEmpty() == false));
-
-        assertThat("the archive store instance has a valid id",
-                (asi.getId() != null && asi.getId().isEmpty() == false));
-
-        Task newTask = new Task();
-        newTask.setId(UUID.randomUUID().toString());
-        newTask.setChannel(channel);
-        newTask.setTaskType(TaskType.DISCOVER);
-        taskManager.submit(newTask);
 
         Task archiveTask = new Task();
         archiveTask.setId(UUID.randomUUID().toString());
@@ -145,10 +117,7 @@ public class LocalFSInformationStoreTest {
         assertThat("We have discovered " + objectsDiscovered + " simple objects", objectsDiscovered > 0);
         assertThat("We have ingested " + objectsIngested + " simple objects", objectsIngested > 0);
         assertThat("We have discovered and ingested the same number of simple objects",
-                   objectsDiscovered,
-                   is(equalTo(objectsIngested)));
-
+                objectsDiscovered,
+                is(equalTo(objectsIngested)));
     }
-
-
 }
