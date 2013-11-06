35769f039059d0b2af7d8885729a79a3d7fb86ad
Joshua Soles <josh@infobelt.com>
2013-11-06 16:35:40 -0500

    Added assertions to the LocalFSInformationStoreTests.

--- stores/src/test/java/org/openskye/stores/information/localfs/LocalFSInformationStoreTest.java
+++ stores/src/test/java/org/openskye/stores/information/localfs/LocalFSInformationStoreTest.java
@@ -14,6 +14,8 @@ import javax.inject.Inject;
 
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -37,6 +39,34 @@ public class LocalFSInformationStoreTest {
         return dis;
     }
 
+
+    public ArchiveStoreDefinition getInMemoryArchive()
+    {
+        ArchiveStoreInstance asi = new ArchiveStoreInstance();
+        asi.setId(UUID.randomUUID().toString());
+        asi.setImplementation(InMemoryArchiveStore.IMPLEMENTATION);
+
+        ArchiveStoreDefinition asd = new ArchiveStoreDefinition();
+        asd.setId(UUID.randomUUID().toString());
+        asd.setArchiveStoreInstance(asi);
+
+        return asd;
+    }
+
+    public ArchiveStoreDefinition getLocalFsArchive(String path)
+    {
+        ArchiveStoreInstance asi = new ArchiveStoreInstance();
+        asi.setId(UUID.randomUUID().toString());
+        asi.setImplementation(LocalFSArchiveStore.IMPLEMENTATION);
+        asi.getProperties().put(LocalFSArchiveStore.LOCALFS_PATH, path);
+
+        ArchiveStoreDefinition asd = new ArchiveStoreDefinition();
+        asd.setId(UUID.randomUUID().toString());
+        asd.setArchiveStoreInstance(asi);
+
+        return asd;
+    }
+
     @Test
     public void createInformationStore() {
         assertThat("We can create an instance of the Local FS information store", registry.build(getLocalFsDis()).isPresent());
@@ -47,17 +77,23 @@ public class LocalFSInformationStoreTest {
         assertThat("Get metadata for the store", registry.build(getLocalFsDis()).get().getMetadata() != null);
         LocalFSInformationStore is = (LocalFSInformationStore) registry.build(getLocalFsDis()).get();
 
-        ArchiveStoreInstance asi = new ArchiveStoreInstance();
-        asi.setImplementation(LocalFSArchiveStore.IMPLEMENTATION);
-        asi.getProperties().put(LocalFSArchiveStore.LOCALFS_PATH, "/tmp/pj2");
         InformationStoreDefinition dis = getLocalFsDis();
-        ArchiveStoreDefinition das = new ArchiveStoreDefinition();
-        das.setArchiveStoreInstance(asi);
+        ArchiveStoreDefinition das = getLocalFsArchive("/tmp/pj2");
         ChannelArchiveStore cas = new ChannelArchiveStore();
         cas.setArchiveStoreDefinition(das);
         Channel channel = new Channel();
         channel.getChannelArchiveStores().add(cas);
         channel.setInformationStoreDefinition(dis);
+        ArchiveStoreInstance asi = das.getArchiveStoreInstance();
+
+        assertThat("the information store definition has a valid id",
+                (dis.getId() != null && dis.getId().isEmpty() == false));
+
+        assertThat("the archive store definition has a valid id",
+                (das.getId() != null && das.getId().isEmpty() == false));
+
+        assertThat("the archive store instance has a valid id",
+                (asi.getId() != null && asi.getId().isEmpty() == false));
 
         Task newTask = new Task();
         newTask.setChannel(channel);
@@ -71,26 +107,46 @@ public class LocalFSInformationStoreTest {
     @Test
     public void ensureWeCanArchiveToALocalFS() {
 
-        ArchiveStoreInstance asi = new ArchiveStoreInstance();
-        asi.setImplementation(InMemoryArchiveStore.IMPLEMENTATION);
         InformationStoreDefinition dis = getLocalFsDis();
-        ArchiveStoreDefinition das = new ArchiveStoreDefinition();
-        das.setArchiveStoreInstance(asi);
+        ArchiveStoreDefinition das = getInMemoryArchive();
         ChannelArchiveStore cas = new ChannelArchiveStore();
         cas.setArchiveStoreDefinition(das);
         Channel channel = new Channel();
         channel.getChannelArchiveStores().add(cas);
         channel.setInformationStoreDefinition(dis);
+        channel.setId(UUID.randomUUID().toString());
+        ArchiveStoreInstance asi = das.getArchiveStoreInstance();
+
+        // Test that the objects have valid ids.
+        assertThat("the information store definition has a valid id",
+                (dis.getId() != null && dis.getId().isEmpty() == false));
+
+        assertThat("the archive store definition has a valid id",
+                (das.getId() != null && das.getId().isEmpty() == false));
+
+        assertThat("the archive store instance has a valid id",
+                (asi.getId() != null && asi.getId().isEmpty() == false));
 
         Task newTask = new Task();
+        newTask.setId(UUID.randomUUID().toString());
         newTask.setChannel(channel);
         newTask.setTaskType(TaskType.DISCOVER);
         taskManager.submit(newTask);
 
         Task archiveTask = new Task();
+        archiveTask.setId(UUID.randomUUID().toString());
         archiveTask.setChannel(channel);
         archiveTask.setTaskType(TaskType.ARCHIVE);
         taskManager.submit(archiveTask);
+
+        long objectsDiscovered = newTask.getStatistics().getSimpleObjectsDiscovered();
+        long objectsIngested = archiveTask.getStatistics().getSimpleObjectsIngested();
+
+        assertThat("We have discovered " + objectsDiscovered + " simple objects", objectsDiscovered > 0);
+        assertThat("We have ingested " + objectsIngested + " simple objects", objectsIngested > 0);
+        assertThat("We have discovered and ingested the same number of simple objects",
+                   objectsDiscovered,
+                   is(equalTo(objectsIngested)));
 
     }
 
