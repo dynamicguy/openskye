4067fd8563794f7bb9b549450af1e6993d400ce2
Matt Taylor <nickylogic@gmail.com>
2013-11-11 09:13:59 -0600

    Task task life cycle with QueueTaskManager

---
+++ core/src/test/java/org/openskye/task/queue/QueueTaskManagerTestModule.java
@@ -0,0 +1,37 @@
+package org.openskye.task.queue;
+
+import com.google.guiceberry.GuiceBerryModule;
+import com.google.inject.AbstractModule;
+import com.google.inject.persist.jpa.JpaPersistModule;
+import org.openskye.metadata.ObjectMetadataRepository;
+import org.openskye.metadata.ObjectMetadataSearch;
+import org.openskye.metadata.impl.InMemoryObjectMetadataSearch;
+import org.openskye.metadata.impl.jpa.JPAObjectMetadataRepository;
+import org.openskye.stores.StoreRegistry;
+import org.openskye.task.TaskManager;
+import org.openskye.task.TaskScheduler;
+import org.openskye.task.quartz.QuartzTaskScheduler;
+
+import java.util.Properties;
+
+public class QueueTaskManagerTestModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        JpaPersistModule jpaPersistModule = new JpaPersistModule("Default");
+        Properties props = new Properties();
+        props.put("javax.persistence.jdbc.url", "jdbc:h2:mem:openskye-test");
+        props.put("javax.persistence.jdbc.user", "sa");
+        props.put("javax.persistence.jdbc.password", "");
+        props.put("javax.persistence.jdbc.driver", "org.h2.Driver");
+        jpaPersistModule.properties(props);
+        install(jpaPersistModule);
+
+        bind(TaskManager.class).to(QueueTaskManager.class).asEagerSingleton();
+        bind(TaskScheduler.class).to(QuartzTaskScheduler.class).asEagerSingleton();
+        bind(StoreRegistry.class).asEagerSingleton();
+        bind(ObjectMetadataRepository.class).to(JPAObjectMetadataRepository.class);
+        bind(ObjectMetadataSearch.class).to(InMemoryObjectMetadataSearch.class);
+
+        install(new GuiceBerryModule());
+    }
+}
