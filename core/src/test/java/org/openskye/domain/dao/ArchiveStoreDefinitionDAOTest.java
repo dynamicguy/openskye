0851ff0966570ab19d8fd240e8fa231d27cf5a03
Krishna <krishna@infobelt.com>
2013-11-14 14:14:39 -0500

    unit tests for the DAO

--- core/src/test/java/org/openskye/domain/dao/ArchiveStoreDefinitionDAOTest.java
+++ core/src/test/java/org/openskye/domain/dao/ArchiveStoreDefinitionDAOTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Testing the {@link ArchiveStoreDefinitionDAO}
+ * //Test the  ArchiveStoreDefinitionDAO
  */
 public class ArchiveStoreDefinitionDAOTest extends AbstractDAOTestBase<ArchiveStoreDefinition> {
 
