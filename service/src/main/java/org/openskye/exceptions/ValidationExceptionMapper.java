3b0dcfe0ebfb2cf8929c4314cafad248d83bca2c
Philip Dodds <philip.dodds@me.com>
2013-11-18 13:10:01 -0500

    Clean up exceptions and fix update method

--- service/src/main/java/org/openskye/exceptions/ValidationExceptionMapper.java
+++ service/src/main/java/org/openskye/exceptions/ValidationExceptionMapper.java
@@ -1,7 +1,5 @@
 package org.openskye.exceptions;
 
-import org.openskye.core.SkyeException;
-
 import javax.validation.ValidationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -12,14 +10,7 @@ import javax.ws.rs.ext.Provider;
 public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
     @Override
     public Response toResponse(ValidationException exception) {
-        String throwingClass = exception.getStackTrace()[0].getClassName();
-        try {
-            Object example = Class.forName(throwingClass).newInstance();
-            ExceptionMessage em = new ExceptionMessage(7000, "Validation error: ", exception.getMessage()+example);
-            return Response.status(Response.Status.BAD_REQUEST).entity(em).type("application/json").build();
-        } catch (Exception e) {
-            throw new SkyeException("Could not create example for exception", e);
-        }
-
+        ExceptionMessage em = new ExceptionMessage(7000, "Validation error: ", exception.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST).entity(em).type("application/json").build();
     }
 }
