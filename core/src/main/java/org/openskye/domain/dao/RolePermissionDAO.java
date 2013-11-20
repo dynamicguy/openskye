2ba3183afddc9db8d12222c47de4d21851ce891b
Joshua Soles <josh@infobelt.com>
2013-11-20 10:32:28 -0500

    Added the formerly missing RolePermissionDAO and RolePermissionResource classes, which allow individual permission values to be created for inclusion on Permission records, which get attached to UserRole records, which are in turn a part of a User.
    ~ Joshua Soles<josh@infobelt.com>

---
+++ core/src/main/java/org/openskye/domain/dao/RolePermissionDAO.java
@@ -0,0 +1,10 @@
+package org.openskye.domain.dao;
+
+import org.openskye.domain.RolePermission;
+
+/**
+ * Interface which manages the storage of RolePermission instances.
+ */
+public class RolePermissionDAO extends AbstractPaginatingDAO<RolePermission>
+{
+}
