da5b3badb7fcdff9a0b76b25574b7c8968557f4c
Philip Dodds <philip.dodds@me.com>
2013-11-27 08:00:58 -0500

    Make sure the constraint violations are returned through the API

--- service/src/main/java/org/openskye/exceptions/ConstraintViolationExceptionMapper.java
+++ service/src/main/java/org/openskye/exceptions/ConstraintViolationExceptionMapper.java
@@ -1,5 +1,6 @@
 package org.openskye.exceptions;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -11,6 +12,12 @@ public class ConstraintViolationExceptionMapper implements ExceptionMapper<Const
     @Override
     public Response toResponse(ConstraintViolationException exception) {
         ExceptionMessage em = new ExceptionMessage(23000, "Constrain violation", exception.getMessage());
+        for(ConstraintViolation violation : exception.getConstraintViolations()) {
+            ExceptionMessageDetail detail = new ExceptionMessageDetail();
+            detail.setPath(violation.getPropertyPath().toString());
+            detail.setMessage(violation.getMessage());
+            em.getDetails().add(detail);
+        }
         return Response.status(Response.Status.BAD_REQUEST).entity(em).type("application/json").build();
     }
 }
