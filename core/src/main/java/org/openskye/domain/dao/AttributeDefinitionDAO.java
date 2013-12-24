603083faed251be52174fd5e0fdab9fee1b54cad
Tyler <kaerfredoc@gmail.com>
2013-12-24 13:42:02 -0500

    JPA Query with join

--- core/src/main/java/org/openskye/domain/dao/AttributeDefinitionDAO.java
+++ core/src/main/java/org/openskye/domain/dao/AttributeDefinitionDAO.java
@@ -1,11 +1,8 @@
 package org.openskye.domain.dao;
 
 import org.openskye.domain.AttributeDefinition;
-import org.openskye.domain.AttributeInstance;
 
-import javax.persistence.EntityManager;
-import javax.persistence.criteria.*;
-import java.util.List;
+import javax.persistence.Query;
 
 /**
  * DAO for the {@link org.openskye.domain.Channel}
@@ -14,18 +11,11 @@ public class AttributeDefinitionDAO extends AbstractPaginatingDAO<AttributeDefin
 
     public boolean isInUse(AttributeDefinition definition)
     {
-        EntityManager em = getEntityManagerProvider().get();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<AttributeInstance> cq = cb.createQuery(AttributeInstance.class);
-        Root<AttributeInstance> root = cq.from(AttributeInstance.class);
-        Join<AttributeInstance, AttributeDefinition> joinDefinition = root.join("attributeDefinition");
+        Query query = currentEntityManager().createQuery("SELECT count(*) FROM AttributeInstance ai " +
+                "JOIN ai.attributeDefinition ad " +
+                "where ad = :attributeDefinition");
 
-        cq.select(root);
-        cq.where(cb.equal(joinDefinition.get("id"), definition.getId()));
-
-        List<AttributeInstance> attributeInstanceList = em.createQuery(cq).getResultList();
-
-        return (attributeInstanceList.size() > 0) ? true : false;
+        return ((Long) query.setParameter("attributeDefinition", definition).getSingleResult() > 0);
     }
 }
 
