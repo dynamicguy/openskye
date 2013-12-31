e06609944d3bb6a1a14e26c715638a7fe961a7b3
Romain L <romain.lheritier@gmail.com>
2013-12-31 10:47:16 -0500

    backing out c3p0... Incompatibility with jdbc 4 in some parts :(

--- service/src/main/java/org/openskye/SkyeApplication.java
+++ service/src/main/java/org/openskye/SkyeApplication.java
@@ -80,15 +80,15 @@ public class SkyeApplication extends Application<SkyeConfiguration> {
         props.put("javax.persistence.jdbc.driver", configuration.getDatabaseConfiguration().getDriverClass());
         props.put("hibernate.dialect", configuration.getDatabaseConfiguration().getDialect());
         
-        props.put("hibernate.connection.provider_class", configuration.getDatabaseConfiguration().getConnectionProviderClass());
-        props.put("hibernate.c3p0.max_size", configuration.getDatabaseConfiguration().getPoolMaxSize());
-        props.put("hibernate.c3p0.min_size", configuration.getDatabaseConfiguration().getPoolMinSize());
-        props.put("hibernate.c3p0.timeout", configuration.getDatabaseConfiguration().getPoolTimeOut());
-        props.put("hibernate.c3p0.max_statements", configuration.getDatabaseConfiguration().getPoolMaxStatements());
-        props.put("hibernate.c3p0.idle_test_period", configuration.getDatabaseConfiguration().getPoolIdleTestPeriod());
-        props.put("hibernate.connection.pool_size", configuration.getDatabaseConfiguration().getConnectionPoolSize());
-        props.put("hibernate.c3p0.preferredTestQuery", configuration.getDatabaseConfiguration().getPoolPreferredTestQuery());
-        props.put("hibernate.c3p0.testConnectionOnCheckout", configuration.getDatabaseConfiguration().getPoolTestConnectionOnCheckout());
+        //props.put("hibernate.connection.provider_class", configuration.getDatabaseConfiguration().getConnectionProviderClass());
+        //props.put("hibernate.c3p0.max_size", configuration.getDatabaseConfiguration().getPoolMaxSize());
+        //props.put("hibernate.c3p0.min_size", configuration.getDatabaseConfiguration().getPoolMinSize());
+        //props.put("hibernate.c3p0.timeout", configuration.getDatabaseConfiguration().getPoolTimeOut());
+        //props.put("hibernate.c3p0.max_statements", configuration.getDatabaseConfiguration().getPoolMaxStatements());
+        //props.put("hibernate.c3p0.idle_test_period", configuration.getDatabaseConfiguration().getPoolIdleTestPeriod());
+        //props.put("hibernate.connection.pool_size", configuration.getDatabaseConfiguration().getConnectionPoolSize());
+        //props.put("hibernate.c3p0.preferredTestQuery", configuration.getDatabaseConfiguration().getPoolPreferredTestQuery());
+        //props.put("hibernate.c3p0.testConnectionOnCheckout", configuration.getDatabaseConfiguration().getPoolTestConnectionOnCheckout());
 
         jpaPersistModule.properties(props);
 
