fc79554d06c90b1bd15b5fcf5f6a0e77ec312a50
Joshua Soles <josh@infobelt.com>
2014-01-03 16:15:20 -0500

    Ensured that Permission objects have a required field for a Shiro permission.

--- core/src/main/java/org/openskye/domain/Permission.java
+++ core/src/main/java/org/openskye/domain/Permission.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -24,7 +26,11 @@ public class Permission implements Identifiable {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     @Column(unique = true, length = 36)
     private String id;
+
+    @NotNull
+    @NotBlank
     private String permission;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "permission")
     @JsonIgnore
     private List<RolePermission> rolePermissions;
