5bfb983322e9742d80f2a0b07c3ffdde4261057c
Philip Dodds <philip.dodds@me.com>
2014-01-10 07:10:55 -0500

    Fix missing import

--- core/src/main/java/org/openskye/domain/ArchiveStoreInstance.java
+++ core/src/main/java/org/openskye/domain/ArchiveStoreInstance.java
@@ -1,5 +1,6 @@
 package org.openskye.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
