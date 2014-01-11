1f1c14efbdc1556b19259c092754f561a021260c
Matt Taylor <matt@infobelt.com>
2014-01-11 16:19:03 -0600

    An archive put/get test that should pass but does not, @Ignore to be removed after ORION-240 is resolved

--- stores/src/test/java/org/openskye/stores/archive/localfs/LocalFSArchiveStoreTest.java
+++ stores/src/test/java/org/openskye/stores/archive/localfs/LocalFSArchiveStoreTest.java
@@ -2,6 +2,7 @@ package org.openskye.stores.archive.localfs;
 
 import com.google.common.base.Optional;
 import com.google.guiceberry.junit4.GuiceBerryRule;
+import com.google.inject.Injector;
 import com.google.inject.persist.PersistService;
 import org.eobjects.metamodel.UpdateCallback;
 import org.eobjects.metamodel.UpdateScript;
@@ -11,29 +12,33 @@ import org.eobjects.metamodel.schema.ColumnType;
 import org.eobjects.metamodel.schema.Schema;
 import org.eobjects.metamodel.schema.Table;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.openskye.core.ArchiveStore;
-import org.openskye.core.QueryContext;
-import org.openskye.core.QueryableStore;
-import org.openskye.core.StructuredObject;
+import org.openskye.core.*;
 import org.openskye.core.structured.Row;
 import org.openskye.domain.*;
 import org.openskye.metadata.ObjectMetadataRepository;
 import org.openskye.stores.StoreRegistry;
 import org.openskye.stores.information.InMemoryTestModule;
 import org.openskye.stores.information.jdbc.JDBCStructuredInformationStore;
+import org.openskye.stores.information.localfs.LocalFileUnstructuredObject;
+import org.openskye.stores.inmemory.InMemoryUnstructuredObject;
 import org.openskye.task.TaskManager;
 import org.openskye.task.step.ArchiveTaskStep;
 import org.openskye.task.step.DiscoverTaskStep;
 
 import javax.inject.Inject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Test basic functionality of the {@link LocalFSArchiveStore}
@@ -50,6 +55,8 @@ public class LocalFSArchiveStoreTest {
     public ObjectMetadataRepository omr;
     @Inject
     public PersistService persistService;
+    @Inject
+    private Injector injector;
 
     @Before
     public void setUp() {
@@ -192,5 +199,44 @@ public class LocalFSArchiveStoreTest {
         assertThat("result has first row", iter.next() != null);
         assertThat("we have a row", iter.hasNext());
         assertThat("result has second row", iter.next() != null);
+    }
+
+    @Test
+    @Ignore  //TODO -- execute this test when issue ORION-240 is resolved
+    public void UnstructuredObjectTest() throws Exception {
+
+        // Construct a mock archive store
+        ArchiveStoreInstance asi = new ArchiveStoreInstance();
+        asi.setImplementation(LocalFSArchiveStore.IMPLEMENTATION);
+        Path temp = Files.createTempDirectory("archive-" + UUID.randomUUID().toString());
+        asi.getProperties().put(LocalFSArchiveStore.LOCALFS_PATH, temp.toAbsolutePath().toString());
+        ArchiveStoreDefinition das = new ArchiveStoreDefinition();
+        das.setId(UUID.randomUUID().toString());
+        das.setArchiveStoreInstance(asi);
+        LocalFSArchiveStore store = new LocalFSArchiveStore();
+        store.initialize(asi);
+        injector.injectMembers(store);
+
+        // Build an unstructured simple object residing in memory
+        Project project = new Project();
+        project.setName("UnstructuredObjectTest Project");
+        project.setId(UUID.randomUUID().toString());
+        String objectId = UUID.randomUUID().toString();
+        String path = "UnstructuredObjectTest/123.txt";
+        String contentIn = "This is a test of unstructured object archiving";
+        InMemoryUnstructuredObject objectIn = new InMemoryUnstructuredObject(project,objectId,path,contentIn);
+
+        // Put the object into the store
+        ArchiveStoreWriter writer = store.getWriter(null);
+        SimpleObject objectPut = writer.put(objectIn);
+        writer.close();
+
+        // Get the object back out of the store and read it
+        ObjectMetadata omPut = objectPut.getObjectMetadata();
+        LocalFileUnstructuredObject objectOut = (LocalFileUnstructuredObject) store.getSimpleObject(omPut).get();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(objectOut.getContent()) );
+        String contentOut = reader.readLine();
+
+        assertThat("Content retrieved matches content stored", contentOut, is(equalTo(contentIn)));
     }
 }
