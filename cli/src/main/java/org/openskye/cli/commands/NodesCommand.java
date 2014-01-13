b999d3404e8c33d2b61e193e2ef30e8ad934ca00
Matt Taylor <matt@infobelt.com>
2014-01-13 09:04:57 -0600

    Add nodes command to CLI

---
+++ cli/src/main/java/org/openskye/cli/commands/NodesCommand.java
@@ -0,0 +1,34 @@
+package org.openskye.cli.commands;
+
+import com.beust.jcommander.Parameters;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+import org.openskye.cli.commands.fields.Field;
+import org.openskye.cli.commands.fields.FieldBuilder;
+import org.openskye.cli.commands.fields.TextField;
+import org.openskye.domain.Node;
+
+import java.util.List;
+
+/**
+ * A command to manage the {@link org.openskye.domain.Node}s.
+ */
+@Parameters(commandDescription = "Manage nodes")
+@Data
+@Slf4j
+@EqualsAndHashCode(callSuper = false)
+public class NodesCommand extends AbstractCrudCommand {
+
+    private final String commandName = "nodes";
+
+    public List<Field> getFields() {
+        return FieldBuilder.start().add(new TextField("hostName")).build();
+    }
+
+    @Override
+    public Class getClazz() {
+        return Node.class;
+    }
+
+}
