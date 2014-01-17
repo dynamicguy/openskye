618414ed1badbceb5ddabfba5f36e3978fd1fb77
Matt Taylor <matt@infobelt.com>
2014-01-17 17:24:39 -0600

    collect node parameter in CLI for discover, archive and cull tasks

--- cli/src/main/java/org/openskye/cli/commands/AbstractTaskStepCommand.java
+++ cli/src/main/java/org/openskye/cli/commands/AbstractTaskStepCommand.java
@@ -110,6 +110,7 @@ public abstract class AbstractTaskStepCommand extends ExecutableCommand {
     public void discover() {
         TaskStep step = new DiscoverTaskStep();
         output.message("Creating a new " + step.getLabel() + " task:\n");
+        selectReferenceField(new ReferenceField(Node.class), step);
         selectReferenceField(new ReferenceField(Channel.class), step);
         create(step);
     }
@@ -117,6 +118,7 @@ public abstract class AbstractTaskStepCommand extends ExecutableCommand {
     public void archive() {
         TaskStep step = new ArchiveTaskStep();
         output.message("Creating a new " + step.getLabel() + " task:\n");
+        selectReferenceField(new ReferenceField(Node.class), step);
         selectReferenceField(new ReferenceField(Channel.class), step);
         create(step);
     }
@@ -124,6 +126,7 @@ public abstract class AbstractTaskStepCommand extends ExecutableCommand {
     public void cull() {
         TaskStep step = new CullTaskStep();
         output.message("Creating a new " + step.getLabel() + " task:\n");
+        selectReferenceField(new ReferenceField(Node.class), step);
         selectReferenceField(new ReferenceField(Project.class), step);
         create(step);
     }
@@ -156,7 +159,6 @@ public abstract class AbstractTaskStepCommand extends ExecutableCommand {
         TaskStep step = new ExtractTaskStep();
         output.message("Creating a new " + step.getLabel() + " task:\n");
         selectReferenceField(new ReferenceField(Node.class), step);
-        //TODO extract should optionally take a set ID parameter instead of a channel
         if (dynamicParams.get("objectSetId") != null) {
             ((ExtractTaskStep) step).setObjectSetId(dynamicParams.get("objectSetId"));
         } else {
