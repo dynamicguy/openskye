a99aa01c3c4ca648537178cba8d2b24d594ae55e
Amanda Mostafavi <amanda@infobelt.com>
2014-02-06 14:03:35 -0500

    Fix for ORION-349, Replication from the command line (allows ReplicateTaskStep to be deserialized)

--- core/src/main/java/org/openskye/domain/Task.java
+++ core/src/main/java/org/openskye/domain/Task.java
@@ -90,7 +90,8 @@ public class Task implements Identifiable {
         @JsonSubTypes.Type(value=org.openskye.task.step.DiscoverTaskStep.class, name="DiscoverTaskStep"),
         @JsonSubTypes.Type(value=org.openskye.task.step.ExtractTaskStep.class, name="ExtractTaskStep"),
         @JsonSubTypes.Type(value=org.openskye.task.step.TestTaskStep.class, name="TestTaskStep"),
-        @JsonSubTypes.Type(value=org.openskye.task.step.VerifyTaskStep.class, name="VerifyTaskStep")
+        @JsonSubTypes.Type(value=org.openskye.task.step.VerifyTaskStep.class, name="VerifyTaskStep"),
+        @JsonSubTypes.Type(value=org.openskye.task.step.ReplicateTaskStep.class, name="ReplicateTaskStep")
     })
     private TaskStep step;
     @Transient
