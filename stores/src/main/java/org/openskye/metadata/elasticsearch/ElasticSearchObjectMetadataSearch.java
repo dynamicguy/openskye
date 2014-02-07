f507d25766d5b02e06f81df044c5ad2da6119850
jbobbysoles <jbobbysoles@me.com>
2014-02-06 21:52:48 -0500

    Added delete methods to ObjectMetadataSearch implementations, by domain, by project, or for an individual ObjectMetadata instance.
    Added bulk index and delete methods to the ObjectMetadataSearch implementations, which are called by supplying a collection that implements the Iterable<ObjectMetadata> interface.
    
    Debug messages for index and bulk index operations, now contain line breaks for readability.

--- stores/src/main/java/org/openskye/metadata/elasticsearch/ElasticSearchObjectMetadataSearch.java
+++ stores/src/main/java/org/openskye/metadata/elasticsearch/ElasticSearchObjectMetadataSearch.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.indices.status.IndicesStatusResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.count.CountResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
@@ -189,7 +191,12 @@ public class ElasticSearchObjectMetadataSearch implements ObjectMetadataSearch {
     public void index(ObjectMetadata objectMetadata) {
         String domainId = objectMetadata.getProject().getDomain().getId();
         String projectId = objectMetadata.getProject().getId();
-        log.debug("Starting to index " + objectMetadata +" on index "+domainId+" with type "+projectId);
+        String message = "Attempting to index an ObjectMetadata instance.\n" +
+                        "Domain Id: " + domainId + "\n" +
+                        "Project Id: " + projectId + "\n" +
+                        "ObjectMetadata: " + objectMetadata;
+        log.debug(message);
+
 
         try {
             String json = this.objectMapper.writeValueAsString(objectMetadata);
@@ -202,17 +209,132 @@ public class ElasticSearchObjectMetadataSearch implements ObjectMetadataSearch {
         }
     }
 
+    @Override
+    public void index(Iterable<ObjectMetadata> objectMetadataList)
+    {
+        // Create a bulk request.
+        log.debug("Creating a bulk index request.");
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        for(ObjectMetadata objectMetadata : objectMetadataList)
+        {
+            try
+            {
+                // For each object in the list, attempt to serialize that object.
+                String domainId = objectMetadata.getProject().getDomain().getId();
+                String projectId = objectMetadata.getProject().getId();
+                String message = "Attempting to add an ObjectMetadata instance to a bulk index request.\n" +
+                        "Domain Id: " + domainId + "\n" +
+                        "Project Id: " + projectId + "\n" +
+                        "ObjectMetadata: " + objectMetadata;
+                log.debug(message);
+
+                String json = objectMapper.writeValueAsString(objectMetadata);
+
+                // Add the index request to the bulk request.
+                bulkRequest.add(client.prepareIndex(domainId, projectId, objectMetadata.getId())
+                        .setSource(json));
+            }
+            catch (JsonProcessingException ex)
+            {
+                throw new SkyeException("Failed to marshal ObjectMetadata as JSON", ex);
+            }
+        }
+
+        // Execute the bulk index request.
+        log.debug("Executing the bulk index request.");
+        BulkResponse response = bulkRequest.execute().actionGet();
+
+        // Upon completion, if there are failures, indicate the nature of each in an exception.
+        if(response.hasFailures())
+            throw new SkyeException(response.buildFailureMessage());
+    }
+
     /**
      * Clears the indexed items for the OMS.
      * <p/>
      * This is intended for testing and demo purposes.
      */
     @Override
-    public void clear() {
-        this.client.prepareDeleteByQuery(this.getIndexNames())
+    public void clear()
+    {
+        log.debug("Clearing all indexed data across all indexed domains.");
+        this.client.prepareDeleteByQuery()
+                .setIndices(getIndexNames())
                 .setQuery(QueryBuilders.matchAllQuery())
                 .execute()
                 .actionGet();
+    }
+
+    @Override
+    public void delete()
+    {
+        log.debug("Deleting indexed data for the domain " + session.getDomain());
+        String domainId = session.getDomain().getId();
+
+        client.prepareDeleteByQuery()
+                .setIndices(domainId)
+                .setQuery(QueryBuilders.matchAllQuery())
+                .execute()
+                .actionGet();
+    }
+
+    @Override
+    public void delete(ObjectMetadata objectMetadata)
+    {
+        log.debug("Deleting indexed information for the ObjectMetadata " + objectMetadata);
+        String domainId = objectMetadata.getProject().getDomain().getId();
+        String projectId = objectMetadata.getProject().getId();
+
+        client.prepareDelete(domainId, projectId, objectMetadata.getId())
+                .execute()
+                .actionGet();
+    }
+
+    @Override
+    public void delete(Iterable<ObjectMetadata> objectMetadataList)
+    {
+        // Create the bulk delete request.
+        log.debug("Creating bulk delete request.");
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        for(ObjectMetadata objectMetadata : objectMetadataList)
+        {
+            // Add the individual delete request to the bulk request.
+            String domainId = objectMetadata.getProject().getDomain().getId();
+            String projectId = objectMetadata.getProject().getId();
+            String message = "Attempting to add an ObjectMetadata to a bulk delete request.\n" +
+                             "Domain Id: " + domainId + "\n" +
+                             "Project Id: " + projectId + "\n" +
+                             "ObjectMetadata: " + objectMetadata;
+            log.debug(message);
+
+            bulkRequest.add(client.prepareDelete(domainId, projectId, objectMetadata.getId()));
+        }
+
+        // Executing the bulk delete request.
+        log.debug("Executing the bulk delete request.");
+        BulkResponse response = bulkRequest.execute().actionGet();
+
+        // Upon completion, if there are failures, indicate them in an exception.
+        if(response.hasFailures())
+            throw new SkyeException(response.buildFailureMessage());
+
+    }
+
+    @Override
+    public void delete(Project project)
+    {
+        String domainId = project.getDomain().getId();
+        String message = "Deleting all indexed data from project " + project;
+        log.debug(message);
+
+        client.prepareDeleteByQuery()
+              .setIndices(domainId)
+              .setTypes(project.getId())
+              .setQuery(QueryBuilders.matchAllQuery())
+              .execute()
+              .actionGet();
     }
 
     /**
